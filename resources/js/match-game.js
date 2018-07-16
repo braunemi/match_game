b3310ff84982671159b3fbe41dacb60450bb7513
Emilio <braunemi@mail.gvsu.edu>
2018-07-16 10:24:38 -0400

    Update generateCardValues()

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -17,13 +17,9 @@ $(document).ready(function() {
 
 MatchGame.generateCardValues = function () {
   var cards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
-  var i = 0
-    , j = 0
-    , temp = null
-
-  for (i = cards.length - 1; i > 0; i -= 1) {
-    j = Math.floor(Math.random() * (i + 1))
-    temp = cards[i]
+  for (let i = 0; i < cards.length; i++) {
+    let j = Math.floor(Math.random() * (cards.length - i))
+    let temp = cards[i]
     cards[i] = cards[j]
     cards[j] = temp
   }
